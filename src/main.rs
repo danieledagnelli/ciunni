966a3a668494de0506c2e4a4f560cc718e2c54db
Daniele D'Agnelli <daniele@bidimensional.net>
2024-08-25 19:32:40 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,31 @@
-use log::Level;
-use console_log;
-use wasm_bindgen_futures;
-use eframe::egui;
+use std::fs::File;
+use std::io::Read;
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result<()> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|cc| {
-            // This gives us image support:
-            egui_extras::install_image_loaders(&cc.egui_ctx);
+fn main() {
+    let mut file = File::open("C:\\Users\\danie\\OneDrive\\books\\Hoffman, Kevin - Programming WebAssembly with Rust_ Unified Development for Web, Mobile, and Embedded Applications (2019, Pragmatic Programmers, LLC, The) - libgen.li.pdf")
+        .expect("Failed to open file");
 
-            Ok(Box::<MyApp>::default())
-        }),
-    )
-}
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)
+        .expect("Failed to read file");
 
-#[cfg(target_arch = "wasm32")]
-fn main() -> eframe::Result<()> {
-    console_log::init_with_level(Level::Debug).expect("error initializing logger");
+    // println!("{:?}", contents);
 
-    let runner = eframe::WebRunner::new();
-    wasm_bindgen_futures::spawn_local(async move {
-        runner.start(
-            "the_canvas_id", // hardcode it in your index.html
-            eframe::WebOptions::default(),
-            Box::new(|cc| {
-                // This gives us image support:
-                egui_extras::install_image_loaders(&cc.egui_ctx);
-
-                Ok(Box::<MyApp>::default())
-            }),
-        ).await.expect("Failed to start eframe web runner");
-    });
-
-    Ok(())
-}
-
-struct MyApp {
-    name: String,
-    age: u32,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
+    if is_pdf(&contents) {
+        println!("The file is a PDF");
+    } else {
+        println!("The file is not a PDF");
     }
+
+    println!("First 4 bytes of contents: {:02X?}", &contents[..4]);
 }
 
-impl App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(format!("Hello, {}!", self.name));
-            ui.label(format!("You are {} years old.", self.age));
-        });
+fn is_pdf(contents: &[u8]) -> bool {
+    if contents.len() < 4 {
+        return false;
     }
+
+    let magic_number: [u8; 4] = [0x25, 0x50, 0x44, 0x46]; // "%PDF" in hexadecimal
+
+    contents[..4] == magic_number
 }
